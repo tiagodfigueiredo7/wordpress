f5659959cf3ee167ec10e43de67b2541085fcaa8
tiagodfigueired7 <tiagod.figueiredo@gmail.com>
2022-11-04 10:08:35 -0300

    teste

--- k8s/efs-csi-driver.tf
+++
@@ -1,9 +0,0 @@
-#
-## AWS EFS CSI Driver
-#
-resource "helm_release" "aws-efs-csi-driver" {
-  name       = "aws-efs-csi-driver"
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-  chart      = "aws-efs-csi-driver"
-  namespace  = "kube-system"
-}
